1ca933b69eba5d93840afb29948765c566f86688
mpnmateus <mpnmateus@gmail.com>
2026-08-26 10:11:09 -0300

    feat(api): add status and dynamic topic routes

--- src/server.js
+++ src/server.js
@@ -12,6 +12,22 @@ app.get("/sobre", (req, res) => {
     res.send("DevStudy - Organizador de estudos para desenvolvedores.");
 });
 
+app.get("/status", (req, res) =>{
+    res.json({
+        aplicação: "DevStudy API",
+        status: "online"
+    });
+});
+
+app.get("/topicos/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    res.json({
+        id: id
+    })
+});
+
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
